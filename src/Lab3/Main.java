e2b37ff3609645c121679668e23bbd901cd81d8e
PC-GAMING <s3462315@rmit.edu.vn>
2020-07-15 01:37:51 +0700

    Linear Equation

--- src/Lab3/Main.java
+++ src/Lab3/Main.java
@@ -2,7 +2,7 @@ package Lab3;
 
 public class Main {
     public static void main(String[] args) {
-        LinearEquation LE = new LinearEquation(1,2,0,0,5,6);
+        LinearEquation LE = new LinearEquation(1,2,3,4,5,6);
         if (LE.getA()*LE.getD() - LE.getB()*LE.getC() == 0) {
             System.out.println("The equation has no solution");
         } else {
